123a2eb6a98151e8b121710ad5304615aebb7387
Warinya <wariroja@gmail.com>
2020-02-16 17:01:07 -0800

    Updates

---
+++ precache-manifest.7abf4018cceaf1e0a4cf503c52e0d2db.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9126fcb94be29c63835b1a465d6e16c1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2619ba21682ff03e23b6",
+    "url": "/portfolio/static/css/main.2de4bf50.chunk.css"
+  },
+  {
+    "revision": "a9709c80cf2fa3846743",
+    "url": "/portfolio/static/js/2.4b163fea.chunk.js"
+  },
+  {
+    "revision": "2619ba21682ff03e23b6",
+    "url": "/portfolio/static/js/main.3c728a9f.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "7257a1a94be550133f3233fd792ee724",
+    "url": "/portfolio/static/media/avatar.7257a1a9.jpg"
+  },
+  {
+    "revision": "6b1983a26a32647c4832456dc9f5dcd3",
+    "url": "/portfolio/static/media/bg1.6b1983a2.jpg"
+  },
+  {
+    "revision": "e643a6cf77b4635dda856f10fa9b7db4",
+    "url": "/portfolio/static/media/bg11.e643a6cf.jpg"
+  },
+  {
+    "revision": "f807b37b1736e7709918ea35ef80ca7d",
+    "url": "/portfolio/static/media/bg3.f807b37b.jpg"
+  },
+  {
+    "revision": "4bff5d45104e2e6c59b1825b54864d58",
+    "url": "/portfolio/static/media/bg4.4bff5d45.jpg"
+  },
+  {
+    "revision": "056eb0586182a8d28273ad11c028e912",
+    "url": "/portfolio/static/media/bg5.056eb058.jpg"
+  },
+  {
+    "revision": "d7d1aaf71d275992031bd75cdb6ec65c",
+    "url": "/portfolio/static/media/bg6.d7d1aaf7.jpg"
+  },
+  {
+    "revision": "607a4a36d058e55773625ba23518e291",
+    "url": "/portfolio/static/media/bg7.607a4a36.jpg"
+  },
+  {
+    "revision": "6737ce3b2577a1c3977e4d479511f642",
+    "url": "/portfolio/static/media/bg8.6737ce3b.jpg"
+  },
+  {
+    "revision": "46605f47dc9c1059deacc38df0ae6408",
+    "url": "/portfolio/static/media/djMcD.46605f47.png"
+  },
+  {
+    "revision": "089c4f4e55f158a69d0364c6b6339f37",
+    "url": "/portfolio/static/media/eva.089c4f4e.jpg"
+  },
+  {
+    "revision": "ffcc99c42164b65da7af6307b66379cf",
+    "url": "/portfolio/static/media/header.ffcc99c4.jpg"
+  },
+  {
+    "revision": "a76c7b4ce725bb07b1572699a5a78c62",
+    "url": "/portfolio/static/media/hero-image-1.a76c7b4c.png"
+  },
+  {
+    "revision": "9616730d099782a3ea6c12c70aa181af",
+    "url": "/portfolio/static/media/hero-image-2.9616730d.png"
+  },
+  {
+    "revision": "02cd0e2d2b27926d1ed5c31a00dd26fa",
+    "url": "/portfolio/static/media/hero-image-3.02cd0e2d.png"
+  },
+  {
+    "revision": "138381e51c42397348a82b448b5fb61b",
+    "url": "/portfolio/static/media/julie.138381e5.jpg"
+  },
+  {
+    "revision": "f8fba45478e18757e67c37b6644a3cec",
+    "url": "/portfolio/static/media/landing.f8fba454.jpg"
+  },
+  {
+    "revision": "2e90c9c26b5f6afbe7f72ae398b6e2dc",
+    "url": "/portfolio/static/media/locali.2e90c9c2.png"
+  },
+  {
+    "revision": "b21019516fd574253e2354080dc1dd00",
+    "url": "/portfolio/static/media/login.b2101951.jpg"
+  },
+  {
+    "revision": "93264f57147e283692f05e7aec57d5e7",
+    "url": "/portfolio/static/media/now-logo.93264f57.png"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/portfolio/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/portfolio/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/portfolio/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/portfolio/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "225653f9f4dacef3e81bcc52f5ef547d",
+    "url": "/portfolio/static/media/profile.225653f9.jpg"
+  },
+  {
+    "revision": "f217f346c88e5fb2720ca7c28827ff72",
+    "url": "/portfolio/static/media/ryan.f217f346.jpg"
+  },
+  {
+    "revision": "6d738c12a1202cfc8af64f0fdf79e859",
+    "url": "/portfolio/static/media/shama.6d738c12.png"
+  }
+]);
